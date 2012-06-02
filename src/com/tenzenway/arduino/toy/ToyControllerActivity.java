81d0326de4cd173004c7ad68045775b9e4bc9b36
Birdming Lai <birdming22@hotmail.com>
2012-06-02 09:26:47 +0800

    new android project

---
+++ src/com/tenzenway/arduino/toy/ToyControllerActivity.java
@@ -0,0 +1,13 @@
+package com.tenzenway.arduino.toy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ToyControllerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
