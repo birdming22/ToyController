4204bb9c466f4ae0d73afcf79caa1264063c3cf7
Birdming Lai <birdming22@hotmail.com>
2012-06-10 16:50:45 +0800

    add UI

--- src/com/tenzenway/arduino/toy/ToyControllerActivity.java
+++ src/com/tenzenway/arduino/toy/ToyControllerActivity.java
@@ -1,13 +1,47 @@
 package com.tenzenway.arduino.toy;
 
+import com.tenzenway.arduino.toy.MainView;
+import com.tenzenway.arduino.toy.R;
+import com.tenzenway.arduino.toy.MainView.ColValues;
+import com.tenzenway.arduino.toy.MainView.JoystickListener;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ToyControllerActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private static final String TAG = "SpokaLightBluetooth_MainActivity";
+
+	public boolean updateColours(int redPerc, int bluePerc, int orangePerc) {
+		byte[] bytes = new byte[1];
+
+		if (redPerc > bluePerc) {
+			bytes[0] = 'I';
+		} else {
+			bytes[0] = 'O';
+		}
+
+		return false;
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// register this activity with the View, to listen to Joystick events
+		((MainView) findViewById(R.id.myMainView))
+				.addJoystickListener(new JoystickListener() {
+					@Override
+					public void onMove(ColValues newColours) {
+						final String result = updateColours(newColours.red,
+								newColours.blue, newColours.orange) ? "OK"
+								: "NOK";
+
+						Log.d(TAG, "Update Colours " + result + " ("
+								+ newColours + ")");
+					}
+				});
+	}
 }
